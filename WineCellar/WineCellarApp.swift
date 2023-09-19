a7215fd61cae3f839f70d106854d7bf737a76315
Christopher Alford <chrisvalford@me.com>
2023-09-19 15:18:50 +0200

    Initial Commit

---
+++ WineCellar/WineCellarApp.swift
@@ -0,0 +1,20 @@
+//
+//  WineCellarApp.swift
+//  WineCellar
+//
+//  Created by Christopher Alford on 19/9/23.
+//
+
+import SwiftUI
+
+@main
+struct WineCellarApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
